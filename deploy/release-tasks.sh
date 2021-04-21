3b8a43fb1cc0fa87b0f056130238a2f702b738eb
Kilderson Sena <dersonsena@gmail.com>
2021-04-21 18:33:39 -0300

    adding npm as engine

--- deploy/release-tasks.sh
+++ deploy/release-tasks.sh
@@ -6,9 +6,8 @@ echo "Running migrations..."
 php artisan migrate
 echo "Done."
 
-echo "Clearing Yii Framework cache..."
+echo "Clearing Framework cache..."
 php artisan config:cache
 php artisan config:clear
 chmod -R 775 storage
-rm -rf ./public/assets/*
 echo "Done."
