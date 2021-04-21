4e8fe96ec0d78ac5672b50e702ddf18fafa67ce5
Kilderson Sena <dersonsena@gmail.com>
2021-04-21 16:15:27 -0300

    updating database settings

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
   //define('HOME_URI', 'http://avaliacoescultivar.com.br');
   define('HOME_URI', 'https://avaliacoescultivar-production.herokuapp.com');
 
-  define('HOSTNAME', 'cis9cbtgerlk68wl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+  define('HOSTNAME', 'pfw0ltdr46khxib3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
   define('DB_NAME', 'qairawn1e3f807di');
   define('DB_USER', 'qufajgtiv3xkykj0');
   define('DB_PASSWORD', 'p0ewdwe1qnr0vc0j');
