86cc7532a578865f71fa1a7b3715313a70a0b253
Kilderson Sena <dersonsena@gmail.com>
2021-05-04 10:28:27 -0300

    deploy configurations

--- deploy/release-tasks.sh
+++ deploy/release-tasks.sh
@@ -2,6 +2,15 @@
 
 set -e
 
+rm -rf ./env && touch ./env
+
+heroku config:get APP_ENV -s  >> .env
+heroku config:get APP_KEY -s  >> .env
+heroku config:get DB_HOST -s  >> .env
+heroku config:get DB_USERNAME -s  >> .env
+heroku config:get DB_PASSWORD -s  >> .env
+heroku config:get DB_DATABASE -s  >> .env
+
 echo "Running migrations..."
 php artisan migrate
 echo "Done."
